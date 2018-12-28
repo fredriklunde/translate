f8e1c2900b6230f4d3c1d1e3add8216ec62b9a17
fredriklunde <fredrik.lunde3@gmail.com>
2018-12-28 09:29:02 +0100

    add flags

--- src/compontents/App.js
+++ src/compontents/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
 class App extends React.Component {
+    state = { language: 'english'}
+
     render() {
-        return <div className="ui container">App</div>
+        return (
+            <div className="ui container">
+                <div>
+                    Select language:
+                    <i className="flag us" />
+                    <i className="flag se" />
+                </div>
+            </div>
+        )
     }
 }
 
