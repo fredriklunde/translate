e1665995ad24f0a306f68c1edce4ace88364fb1d
fredriklunde <fredrik.lunde3@gmail.com>
2018-12-28 09:48:35 +0100

    styling

--- src/compontents/Button.js
+++ src/compontents/Button.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Button extends React.Component {
     render() {
         return (
-            <button className="ui primary">
+            <button className="ui button primary">
                 Submit
             </button>
         )
