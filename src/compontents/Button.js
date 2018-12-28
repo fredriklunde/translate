f96fb597ea4f0b589a349d62f02dd95cd864968c
fredriklunde <fredrik.lunde3@gmail.com>
2018-12-28 09:58:59 +0100

    linking compontents to context

--- src/compontents/Button.js
+++ src/compontents/Button.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import LangageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
+    static contextType = LangageContext;
+
     render() {
+        console.log(this.context);
         return (
             <button className="ui button primary">
                 Submit
